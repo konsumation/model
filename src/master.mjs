620248aa90bc704beb9266ac7a2c95db998bb248
Markus Felten <Markus.Felten@gmx.de>
2024-03-21 22:20:44 +0100

    fix: package name

--- src/master.mjs
+++ src/master.mjs
@@ -1,11 +1,7 @@
-
 export { Category } from "./category.mjs";
 export { Meter } from "./meter.mjs";
 export { Note } from "./note.mjs";
 
 export class Master {
-
-    async initialize(config) {
-        
-    }
+  async initialize(config) {}
 }
---
+++ tests/simple-ava.mjs
@@ -0,0 +1,9 @@
+import test from "ava";
+import { Master } from "@konsumation/model";
+
+test("initialize", t => {
+  const master = new Master();
+  master.initialize({});
+  t.truthy(master);
+});
+
