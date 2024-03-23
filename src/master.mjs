8b1021f4d60db1539b86062585d2af4b6c635746
Markus Felten <Markus.Felten@gmx.de>
2024-03-23 11:41:01 +0100

    fix: declare schemaVersion

--- src/master.mjs
+++ src/master.mjs
@@ -2,10 +2,14 @@ import { Category } from "./category.mjs";
 export { Meter } from "./meter.mjs";
 export { Note } from "./note.mjs";
 export { Category };
+
 /**
- *
+ * @property {string} schemaVersion
  */
 export class Master {
+
+  schemaVersion;
+  
   /**
    *
    * @param {Object|string} config
