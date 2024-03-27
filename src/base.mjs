2ea74dc4a68fa20e51244c82bfc93aef0ecedb0f
Markus Felten <Markus.Felten@gmx.de>
2024-03-27 20:06:00 +0100

    fix: docs

--- src/base.mjs
+++ src/base.mjs
@@ -1,12 +1,25 @@
 export class Base {
+
+  /**
+   * Attribute definitions.
+   * @return {object}
+   */
   static get attributes() {
     return {};
   }
 
+  /**
+   * Maping of attribute names from internal (javascript) to external (database).
+   * @return {object}
+   */
   static get attributeNameMapping() {
     return {};
   }
 
+  /**
+   * Attribute names on the javacript side.
+   * @return {string[]}
+   */
   static get attributeNames() {
     return Object.keys(this.attributes);
   }
@@ -16,6 +29,10 @@ export class Base {
     return this.constructor.attributeNames;
   }
 
+  /**
+   * Object keys are the mapped external attribute names.
+   * @return {object}
+   */
   get attributeValues() {
     // @ts-ignore
     const mapping = this.constructor.attributeNameMapping;
@@ -26,7 +43,10 @@ export class Base {
         .map(name => [mapping[name] || name, this[name]])
     );
   }
-
+  /**
+   * Sets values with external attribute names.
+   * @param {object} values
+   */
   set attributeValues(values) {
     // @ts-ignore
     const mapping = this.constructor.attributeNameMapping;
