a2539fbec782a107679878eb65a47a876d239387
Markus Felten <Markus.Felten@gmx.de>
2024-03-28 18:37:35 +0100

    fix: declare additional attribute properties

--- src/attributes.mjs
+++ src/attributes.mjs
@@ -2,15 +2,19 @@
  * Description of the content.
  */
 export const description = { type: "string", writable: true };
-export const name = { type: "string", writable: true };
-export const id = { type: "number", writable: true };
+export const name = { type: "string", writable: true, mandatory: true };
+export const id = { type: "number", writable: true, isKey: true };
 
 /**
  * Physical unit.
  * @return {string}
  */
 export const unit = { type: "string", writable: true };
-export const schemaVersion = { type: "string", writable: true };
+export const schemaVersion = {
+  type: "string",
+  writable: true,
+  mandatory: true
+};
 export const serial = { type: "string", writable: true };
 export const validFrom = { type: "timestamp", writable: true };
 
@@ -18,4 +22,9 @@ export const validFrom = { type: "timestamp", writable: true };
  * Precission
  * @return {number}
  */
-export const fractionalDigits = { type: "number", default: 2, writable: true };
+export const fractionalDigits = {
+  type: "number",
+  default: 2,
+  writable: true,
+  mandatory: true
+};
