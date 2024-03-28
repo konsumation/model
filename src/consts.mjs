484ac218a01b6097d3a6471f1997c2310256a23e
Markus Felten <Markus.Felten@gmx.de>
2024-03-28 14:28:09 +0100

    fix: declare SCHEMA_VERSION_3

--- src/consts.mjs
+++ src/consts.mjs
@@ -10,6 +10,11 @@ export const SCHEMA_VERSION_1 = "1";
 export const SCHEMA_VERSION_2 = "2";
 
 /**
+ * Values are attached to the meter
+ */
+export const SCHEMA_VERSION_3 = "3";
+
+/**
  * Schema version for newly created databases 
  */
-export const SCHEMA_VERSION_CURRENT = SCHEMA_VERSION_2;
+export const SCHEMA_VERSION_CURRENT = SCHEMA_VERSION_3;
