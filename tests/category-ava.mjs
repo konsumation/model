99a8f4b2c834ddb9133aa6d1e402bb74b89efe9c
Markus Felten <Markus.Felten@gmx.de>
2024-03-28 11:15:35 +0100

    style: reformat

--- tests/category-ava.mjs
+++ tests/category-ava.mjs
@@ -2,7 +2,4 @@ import test from "ava";
 import { testCategoryConstructor } from "@konsumation/db-test";
 import { Category } from "@konsumation/model";
 
-
 test("Category constructor", t => testCategoryConstructor(t, Category));
-
-
