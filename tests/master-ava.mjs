8eaf3f1f438fa892890c2628279bfc237a1e4aed
Markus Felten <Markus.Felten@gmx.de>
2024-04-25 15:44:12 +0200

    test: Master.factories

--- tests/master-ava.mjs
+++ tests/master-ava.mjs
@@ -1,9 +1,29 @@
 import test from "ava";
-import { Master, SCHEMA_VERSION_CURRENT } from "@konsumation/model";
+import {
+  Master,
+  Category,
+  Meter,
+  Note,
+  Value,
+  SCHEMA_VERSION_CURRENT
+} from "@konsumation/model";
 
 test("initialize", async t => {
   const master = await Master.initialize({ description: "hello" });
   t.is(master.schemaVersion, SCHEMA_VERSION_CURRENT);
   t.is(master.description, "hello");
+
   await master.close();
+});
+
+test("factories", t => {
+  const master = new Master({});
+
+  t.deepEqual(master.factories, {
+    master: Master,
+    category: Category,
+    meter: Meter,
+    note: Note,
+    value: Value
+  });
 });
